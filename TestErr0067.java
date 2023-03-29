d3384e2124efea312a68cf10d3e402fb03202793
13020210067_YAYANG NURAVIN NABILAH <127616763+ynabilah@users.noreply.github.com>
2023-03-29 16:25:07 +0800

    MID no. 1

---
+++ TestErr0067.java
@@ -0,0 +1,14 @@
+/* NAMA 	: YAYANG NURAVIN NABILAH
+STAMBUK 	: 13020210067*/
+
+
+public class TestErr0067{
+	public static void main(String[] args) {	
+	int i;
+	int k = 100.0;
+	char c = "s";
+	boolean falses = true;
+	int j = i+1;
+	System.out.println("j adalah "+ j + " k adalah "+ k + " dan c adalah ")+ c + " dan trues adalah "+ falses);
+	}
+}
---
+++ TestErr20067.java
@@ -0,0 +1,21 @@
+/* NAMA 	: YAYANG NURAVIN NABILAH
+STAMBUK 	: 13020210067*/
+
+public class TestErr20067{
+	public static void main(String[] args) {
+	int i;
+	int n;
+	for(int i=0; i<10;i++);
+		tot += i;
+	if (i<j){
+		System.out.println(i);
+	}else{
+		System.out.println(j);
+	}
+	do{
+		System.out.print (i+"\n");
+		j++;
+	} while (j<10);
+		 j++;
+	}
+}

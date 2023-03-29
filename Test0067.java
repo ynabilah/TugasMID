8e94f684c102259a716dba4cbe2caa074f4d74cd
13020210067_YAYANG NURAVIN NABILAH <127616763+ynabilah@users.noreply.github.com>
2023-03-29 16:25:53 +0800

    MID no.2

---
+++ Test0067.java
@@ -0,0 +1,77 @@
+/*NAMA 		: YAYANG NURAVIN NABILAH
+STAMBUK		: 13020210067*/
+
+
+public class Test0067 { 
+    private Double Balok;
+    private Double Balok1;
+    private Double Balok2;
+   
+public Balok(String l,p,t, int 20,35,15){
+this.Balok1 = 0;
+Balok Balok2 = new Balok();
+
+}
+   
+public double getl(){
+    return l;
+}
+public void setl(String l){
+this.l = 5;
+}
+public double getp(){
+    return p;
+}
+public void setp(String p){
+this.p = 25;
+}
+public double getp(t){
+    return Balok;
+}
+public void setSisi(String sisi){
+this.t = BALOK;
+}
+
+(f)tampilkan panjang, lebar, Luas, dan keliling balok dari konstruktor Balok()
+(g)panggil method info dari konstruktor Balok(20,35, 15)
+//class Balok
+class Balok extends PersegiPanjang{
+int t;
+public Balok() {
+(h) berisi nilai panjang & lebar yang diperoleh dari konstruktor superclass
+persegi panjang serta tinggi=15
+}
+public Balok(int p, int l, int t) {
+(i) berisi nilai pada saat runtime p p, l=l dari konstruktor superclass
+persegi panjang t t;
+}
+(j) buat getter setter class balok
+public int volume(){
+(k) untuk menghitung volume balok : p x l x t
+}
+}
+public void info(){
+ (l)tampilkan panjang, lebar, tinggi,Luas, dan volume balok
+
+}
+}
+// class Persegi Panjang
+class PersegiPanjang{
+int p, l;
+public PersegiPanjang() {
+(m) berisi nilai panjang=30 & lebar=20
+}
+public PersegiPanjang(int p, int l) {
+(n) berisi nilai pada saat runtime p p, l=l
+ }
+(o) buat getter setter class Persegi Panjang
+public int luas(){
+(q) untuk menghitung luas persegi panjang : p x l
+ }
+public int keliling(){
+(r) untuk menghitung keliling persegi panjang : 2x (p+l)
+}
+}
+        }
+
+}

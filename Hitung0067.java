c241d692e54a76fada300f0d1dfbd74c3b2359a9
13020210067_YAYANG NURAVIN NABILAH <127616763+ynabilah@users.noreply.github.com>
2023-03-29 16:26:40 +0800

    MID no.4

---
+++ Hitung0067.java
@@ -0,0 +1,17 @@
+/* NAMA 	: YAYANG NURAVIN NABILAH
+STAMBUK 	: 13020210067*/
+
+import java.util.Scanner;
+public class Hitung0067 {
+ 	public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double mil, hasil;
+        double km = 1.60934;
+        System.out.println("berapa Mil : ");
+	mil = input.nextDouble();
+        System.out.println(" hasil ");
+	hasil = mil * km;
+        System.out.println(" " + mil + " Mil = " + hasil + " Km");
+    	}
+    
+}
